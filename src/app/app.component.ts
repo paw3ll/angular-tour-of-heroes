04d24ca2ec05c5d60c51d60d019c9c5fca315e9d
Colton Zeinner <colton.z1995@gmail.com>
2018-12-21 11:10:59 -0500

    Header and Style edits
    Created initial application structure using Angular CLI, Created application title, created basic CSS for remaining application (subject to change)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+@Component(
+{
+  
+selector: 'app-root',
+  
+templateUrl: './app.component.html',
+  
+styleUrls: ['./app.component.css']
+
 })
-export class AppComponent {
-  title = 'app works!';
+
+
+export class AppComponent 
+{
+  
+title = 'Tour of Heroes';
+
 }
